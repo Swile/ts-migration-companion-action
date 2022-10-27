c5307d30eb595672123807370a415a8bc6c520d8
Grégory Houllier <gregory.houllier@gmail.com>
2022-10-27 23:58:45 +0200

    chore(build): remove useless artifacts

--- dist/sorter.d.ts
+++
@@ -1,5 +0,0 @@
-interface Countable {
-    count: number;
-}
-export declare function sortByCount(left: Countable, right: Countable): number;
-export {};
--- dist/tsc-report-comparator.d.ts
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-declare function main(log?: boolean): Promise<string>;
-export default main;
